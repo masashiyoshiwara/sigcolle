d270c10b6e4102a58d095e11ef26899951af15e5
unknown <Administrator@S1502C11693-T1.tisnt.tis.co.jp>
2017-01-23 14:52:57 +0900

    キャンペーン登録用のコントローラを作成

---
+++ src/main/java/net/unit8/sigcolle/controller/RegisterCampaignController.java
@@ -0,0 +1,74 @@
+package net.unit8.sigcolle.controller;
+
+import enkan.component.doma2.DomaProvider;
+import enkan.data.Flash;
+import enkan.data.HttpResponse;
+import enkan.data.Session;
+import kotowari.component.TemplateEngine;
+import net.unit8.sigcolle.dao.CampaignDao;
+import net.unit8.sigcolle.dao.SignatureDao;
+import net.unit8.sigcolle.dao.UserDao;
+import net.unit8.sigcolle.form.CampaignForm;
+import net.unit8.sigcolle.form.SignatureForm;
+import net.unit8.sigcolle.model.Campaign;
+import net.unit8.sigcolle.model.Signature;
+import net.unit8.sigcolle.model.User;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.io.IOException;
+
+import static enkan.util.BeanBuilder.builder;
+import static enkan.util.HttpResponseUtils.RedirectStatusCode.SEE_OTHER;
+import static enkan.util.HttpResponseUtils.redirect;
+import static enkan.util.HttpResponseUtils.resourceResponse;
+import static enkan.util.ThreadingUtils.some;
+
+/**
+ * @author Masashi Yoshiwara
+ */
+public class RegisterCampaignController {
+    @Inject
+    TemplateEngine templateEngine;
+
+    @Inject
+    DomaProvider domaProvider;
+
+    public HttpResponse index(CampaignForm form, Flash flash) throws IOException {
+        Session session = new Session();
+        String name = (String) session.get("userId");
+
+        if(name == null){
+        return templateEngine.render("index",
+                "campaign", new CampaignForm()
+        );
+    }
+        return templateEngine.render("registerCampaign",
+                "campaign", new CampaignForm()
+        );
+    }
+
+    @Transactional
+    public HttpResponse register(CampaignForm form, Session session) throws IOException {
+
+        if (form.hasErrors()) {
+            return templateEngine.render("register",
+                    "campaign", form
+            );
+        }
+
+        CampaignDao campaignDao = domaProvider.getDao(CampaignDao.class);
+
+        Campaign campaign = builder(new Campaign())
+                .set(Campaign::setTitle, form.getTitle())
+                .set(Campaign::setStatement, form.getStatement())
+                .set(Campaign::setGoal, form.getGoal())
+                .set(Campaign::setCreatedBy, form.getCreatedBy())
+                .build();
+        campaignDao.insert(campaign);
+
+        return builder(redirect("/", SEE_OTHER))
+                .set(HttpResponse::setSession, session)
+                .build();
+    }
+}
