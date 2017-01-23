fd92c6c03be0569c20e746374952126f16895e06
hama <hama>
2017-01-23 14:21:21 +0900

    キャンペーン登録機能のためformとrootのクラス変更

--- src/main/java/net/unit8/sigcolle/form/CampaignForm.java
+++ src/main/java/net/unit8/sigcolle/form/CampaignForm.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Max;
 
 /**
  * @author kawasima
@@ -14,7 +15,26 @@ public class CampaignForm extends FormBase {
     @DecimalMax("9999")
     private String campaignId;
 
+    private String title;
+
+    // Markdown description
+    private String statement;
+
+    private String goal;
+
+    private String createdBy;
+
+
     public Long getCampaignIdLong() {
         return Long.parseLong(campaignId);
+    }
+    public String getTitle() {
+        return title;
+    }
+    public Long getGoalLong() {
+        return Long.parseLong(goal);
+    }
+    public Long getCreatedByLong() {
+        return Long.parseLong(createdBy);
     }
 }
